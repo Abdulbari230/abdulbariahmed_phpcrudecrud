c51df66ed4a7c0ff84b07c5ae504eec15075b27c
Abdulbari Ahmed <142043127+Abdulbari230@users.noreply.github.com>
2024-11-07 18:58:41 -0600

    Update credentials.php

--- credentials.php
+++ credentials.php
@@ -1,6 +1,6 @@
 <?php
 // Remember to populate data here
-$servername = 'localhos';     //if apache on diff server from db or using Docker, set IP address
+$servername = 'localhost';     //if apache on diff server from db or using Docker, set IP address
 $dbname = '';              //which db you're going to use
 $username = '';
 $password = '';
